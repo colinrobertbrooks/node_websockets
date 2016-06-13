85f0bb9b445d62c428b3bd6c3b28097e9bd16abf
Colin Cummings <colinrcummings@gmail.com>
2016-06-13 09:21:04 -0400

    refactor index.js

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 ## About
 A chat application built with Sockets.io on Node.js following [this](http://socket.io/get-started/chat/) tutorial.
 
-## Server
-* Run `node index.js` in the directory.
-* Navigate to [localhost:3000/](http://localhost:3000/) in the browser.
+## Development
+* run `$ npm install`.
+* Run `node index.js`.
+* Navigate to [localhost:5000/](http://localhost:5000/) in the browser.
+
+## Production
+The latest version is hosted on Heroku and can be viewed [here](https://node-websockets.herokuapp.com/) (it's on a free web dyno so it may take a second to spin up).
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express');
+var socketIO  = require('socket.io');
+var path = require('path');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+var PORT = process.env.PORT || 5000;
+var INDEX = path.join(__dirname, 'index.html');
+
+var server = express()
+  .use(function(req, res) { res.sendFile(INDEX); })
+  .listen(PORT, function() { console.log('Listening on ' + PORT); });
+
+var io = socketIO(server);
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -14,8 +19,4 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
-});
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
 });
